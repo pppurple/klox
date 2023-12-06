0670af31353eb1f453805845df3ea62d937163a7
pppurple <shuggie.otis0@gmail.com>
2023-12-07 03:09:47 +0900

    fix Environment

--- com/example/klox/Parser.kt
+++ com/example/klox/Parser.kt
@@ -151,7 +151,9 @@ class Parser(
             body = Stmt.Block(listOf(body, Stmt.Expression(increment)))
         }
 
-        if (condition == null) condition = Expr.Literal(true)
+        if (condition == null) {
+            condition = Expr.Literal(true)
+        }
         body = Stmt.While(condition, body)
 
         if (initializer != null) {
